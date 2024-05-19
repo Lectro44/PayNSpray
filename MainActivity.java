5f758361e1730e7ee43b0f17771bc5a80448dbd1
Lectro44 <138900239+Lectro44@users.noreply.github.com>
2024-05-19 12:12:17 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,19 @@
-package com.example.lab5;
+package com.example.lab_6;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Handler;
-import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,28 +21,58 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        // on below line we are calling handler to run a task
-        // for specific time interval
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // on below line we are
-                // creating a new intent
-                Intent i = new Intent(MainActivity.this, MainActivity2.class);
-
-                // on below line we are
-                // starting a new activity.
-                startActivity(i);
-
-                // on the below line we are finishing
-                // our current activity.
-                finish();
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button example = findViewById(R.id.buttonExample);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) CheckBox exampleCheckBox = findViewById(R.id.checkBoxExample);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) RadioGroup exampleRadioGroup = findViewById(R.id.radioGroupExample);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ToggleButton exampletoggleButton = findViewById(R.id.toggleButtonExample);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton exampleImageButton = findViewById(R.id.imageButtonExample);
+        example.setOnClickListener(arg0 -> {
+            // TODO Auto-generated method stub
+            ToastToDisplay("Hey Button is pressed!!");
+        });
+        exampleCheckBox.setOnClickListener(v -> {
+// TODO Auto-generated method stub
+            if (((CheckBox) v).isChecked()) {
+                ToastToDisplay("Check Box is checked");
+            } else {
+                ToastToDisplay("Check box is unchecked");
             }
-        }, 3000);
+        });
+        exampleRadioGroup
+                .setOnCheckedChangeListener((group, checkedId) -> {
+// TODO Auto-generated method stub
+                    RadioButton rb1 = findViewById(R.id.radioButton1);
+                    RadioButton rb2 = findViewById(R.id.radioButton2);
+                    if (rb1.isChecked()) {
+                        ToastToDisplay("Radio Button 1 is checked");
+                    } else if (rb2.isChecked()) {
+                        ToastToDisplay("Radio Button 2 is checked");
+                    } else {
+                        ToastToDisplay("Radio Button 3 is checked");
+                    }
+                });
+        exampletoggleButton.setOnClickListener(v -> {
+        //TODO Auto-generated method stub
+            if (((ToggleButton) v).isChecked()) {
+                ToastToDisplay("Toggle button is ON");
+            } else {
+                ToastToDisplay("Toggle Button is OFF");
+            }
+        });
+        exampleImageButton.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            ToastToDisplay("Image Button is pressed");
+        });
+    }
+    private void ToastToDisplay(String args) {
+        Toast.makeText(getBaseContext(), args, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
